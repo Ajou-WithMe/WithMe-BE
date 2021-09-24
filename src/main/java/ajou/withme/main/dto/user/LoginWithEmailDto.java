b2a55f6ca699c110780a51f06330e8279beb9906
HYOTAEK <hyotaek9812@gmail.com>
2021-09-25 04:36:44 +0900

    fix: test failed fixing

--- src/main/java/ajou/withme/main/dto/user/LoginWithEmailDto.java
+++ src/main/java/ajou/withme/main/dto/user/LoginWithEmailDto.java
@@ -1,8 +1,10 @@
 package ajou.withme.main.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginWithEmailDto {
     private String email;
     private String pwd;
